36c992483bf16828e5fbfa929bab9402578a61fc
Andriy4334 <andriybal4334@gmail.com>
2026-01-15 22:44:25 +0200

    Add files via upload

---
+++ WeatherService.cs
@@ -0,0 +1,41 @@
+﻿using System.Net.Http;
+using System.Threading.Tasks;
+using Newtonsoft.Json;
+
+namespace WarehouseWinForms.Services
+{
+    public class WeatherService
+    {
+        private const string ApiKey = "anonim"; //мій ключ 
+        private const string City = "Lviv";
+        private const string Country = "ua";
+
+        public async Task<double?> GetCurrentTemperatureCelsiusAsync()
+        {
+            string url =
+                "https://api.openweathermap.org/data/2.5/weather?q="
+                + City + "," + Country +
+                "&appid=" + ApiKey +
+                "&units=metric";
+
+            using (var client = new HttpClient())
+            {
+                var json = await client.GetStringAsync(url);
+
+                var data = JsonConvert.DeserializeObject<WeatherResponse>(json);
+
+                return data.main.temp;
+            }
+        }
+    }
+
+    public class WeatherResponse
+    {
+        public MainInfo main { get; set; }
+    }
+
+    public class MainInfo
+    {
+        public double temp { get; set; }
+    }
+}
