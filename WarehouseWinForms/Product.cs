e0b9777d02a751404a265208c370c1c17af978e5
ylowag <380992693288max@gmail.com>
2026-01-15 21:36:52 +0200

    Add files via upload

---
+++ WarehouseWinForms/Product.cs
@@ -0,0 +1,11 @@
+﻿namespace WarehouseWinForms.Models
+{
+    public class Product
+    {
+        public int Id { get; set; }
+        public string Name { get; set; } = "";
+        public int Quantity { get; set; }
+        public decimal Price { get; set; }
+        public decimal Total => Quantity * Price;
+    }
+}
